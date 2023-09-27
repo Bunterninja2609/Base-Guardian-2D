e67e61a54e8d2be17e674b5461c90278eff7ff01
Bunterninja2609 <joshi2609@icloud.com>
2023-09-27 17:06:06 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,25 @@
 -- main.lua
 
 -- Define some variables to keep track of the current state
-local currentState = "offline"
+local state = {}
+state.current = "titlescreen"
 local offline
 local online
 
+
+
 function love.update(dt)
-    if currentState ~= "titlescreen"then
+    if state.current ~= "titlescreen"then
         offline = require("offline")
         online = require("online")
+    end
+end
+function love.draw()
+    love.window.setFullscreen(true, "desktop")
+    love.graphics.setBackgroundColor(0.5,0.5,1)
+end
+function love.keypressed(key)
+    if key == "return" then
+        state.current = "offline"
     end
 end
