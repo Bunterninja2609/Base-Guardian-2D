081d2225cdbb4a35ed9a75c6a6d9162e66882cbe
Bunterninja2609 <joshi2609@icloud.com>
2023-10-11 20:44:32 +0200

    smth idk tho

--- main.lua
+++ main.lua
@@ -5,12 +5,25 @@ local state = {}
 state.current = "titlescreen"
 local offline
 local online
+local width = love.graphics.getWidth()
+local height = love.graphics.getHeight()
+
+function drawMode(x, y, localWidth, localHeight, mode)
+    love.graphics.setColor(0 ,0 ,0 ,0.5)
+    love.graphics.rectangle("fill", x + 10, y + 10, localWidth, localHeight, 30)
+    love.graphics.setColor(1 ,1 ,1 ,1)
+    love.graphics.rectangle("fill", x , y , localWidth, localHeight, 30)
+
+    love.graphics.setColor(0 ,0 ,0 ,1)
+    love.graphics.rectangle("fill", x + 20 , y + 230, localWidth - 40, localHeight - 300, 10)
 
 
+end
 
 function love.update(dt)
     if state.current == "titlescreen"then
-        
+        width = love.graphics.getWidth()
+        height = love.graphics.getHeight()
         
     elseif state.current == "offline" then
         offline = require("offline")
@@ -21,6 +34,11 @@ end
 function love.draw()
     love.window.setFullscreen(true, "desktop")
     love.graphics.setBackgroundColor(0.5,0.5,1)
+    love.graphics.setBackgroundColor(0.5, 0.5, 0.5)
+    drawMode( 50, 50, 550, height - 100, 10, nil)
+    drawMode( width - 50 -550, 50, 550, height - 100, 10, nil)
+    
+    
 end
 function love.keypressed(key)
     if key == "1" then
